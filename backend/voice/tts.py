19525917d7b271148149278bee4f2159a9c47ea3
Shorya1117 <shoryaprajapat@jklu.edu.in>
2026-05-01 11:58:11 +0530

    TTS Issue solve again

--- backend/voice/tts.py
+++ backend/voice/tts.py
@@ -1,14 +1,14 @@
 import pyttsx3
 
-engine = pyttsx3.init()
-
-voices = engine.getProperty('voices')
-
-engine.setProperty('voice', voices[1].id)
-
-engine.setProperty('rate', 190)
-
 def speak(text):
     print(f"Olivia: {text}")
+    
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 190)
+
     engine.say(text)
     engine.runAndWait()
+    engine.stop()
